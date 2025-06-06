c6644b28f1be7e5eea2c97c1304cbc5724a56077
jdwolf <jdwolf2@gmail.com>
2025-06-06 08:30:42 -0400

    added columns

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './style.css'
+// main.js
+import '@syncfusion/ej2-base/styles/material.css'
+import '@syncfusion/ej2-vue-grids/styles/material.css'
+import { registerLicense } from '@syncfusion/ej2-base'
+registerLicense(
+  'Ngo9BigBOggjHTQxAR8/V1NMaF5cXmRCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWX5ednVdRmNcUUJ+Wko='
+)
 
 createApp(App).mount('#app')
-
-// update test
